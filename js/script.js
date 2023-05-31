a2f115d6859d3886ba844ab6911a52a7f18e4dac
Md. Koushik Islam <koushik.post@gmail.com>
2023-05-31 21:59:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Access image by id and change
+            the display property to block*/
+function show() {            
+            document.getElementById('card')
+                .style.display = "block";
+            document.getElementById('btnID')
+                .style.display = "none";
+            }
